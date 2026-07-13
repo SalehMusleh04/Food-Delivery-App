c53c641d4342361048e589f82cc2035844778569
SalehMusleh04 <saleh.abedrabo@gmail.com>
2026-07-13 20:02:38 +0300

    Splash Screen

---
+++ lib/screens/splash.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_delivery_app/screens/onboarding.dart';
+import 'package:food_delivery_app/utils/app_colors.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: controller, curve: Curves.easeIn));
+    controller.forward();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      if (!mounted) return;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Onboarding()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(gradient: AppColors.primaryColor),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Spacer(),
+              FadeTransition(
+                opacity: fadeAnimation,
+                child: SvgPicture.asset('assets/imgs/Foodgo.svg'),
+              ),
+              Spacer(),
+              Expanded(
+                child: Stack(
+                  alignment: Alignment.bottomLeft,
+                  children: [
+                    Positioned(
+                      left: -10,
+                      child: Image.asset(
+                        'assets/imgs/burger2.png',
+                        height: 220,
+                      ),
+                    ),
+                    Positioned(
+                      left: 100,
+                      child: Image.asset(
+                        'assets/imgs/burger1.png',
+                        height: 190,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = LinearGradient(
+    colors: [Color(0xFFFF939B), Color(0xFFEF2A39)],
+    begin: AlignmentDirectional.topStart,
+    end: AlignmentDirectional.bottomEnd,
+  );
+
+  static const secondaryColor = Color(0xFF3C2F2F);
+  static const thirdColor = Color(0xFF1CC019);
+}
