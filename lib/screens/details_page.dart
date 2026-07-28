b5ec4e4da64f8ef166fac103f721411465d1cf23
SalehMusleh04 <saleh.abedrabo@gmail.com>
2026-07-29 01:58:29 +0300

    Fixed part in details page

--- lib/screens/details_page.dart
+++ lib/screens/details_page.dart
@@ -22,129 +22,123 @@ class _DetailsPageState extends State<DetailsPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: CustomScrollView(
-                slivers: [
-                  SliverPadding(
-                    padding: const EdgeInsetsGeometry.all(10),
-                    sliver: SliverAppBar(
-                      surfaceTintColor: Colors.transparent,
-                      pinned: true,
-                      leading: IconButton(
-                        icon: Icon(
-                          Icons.arrow_circle_left_outlined,
-                          size: size.width * 0.1,
-                        ),
-                        onPressed: () => Navigator.pop(context),
-                      ),
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      flexibleSpace: FlexibleSpaceBar(
-                        background: Image.asset(
-                          foods[widget.index].imgPath,
-                          fit: BoxFit.contain,
-                        ),
-                      ),
-                      expandedHeight: size.height * 0.24,
-                      actions: [
-                        FavButton(
-                          isFavorite: foods[widget.index].isFavorite,
-                          onPressed: () => setState(() {
-                            foods[widget.index] = foods[widget.index].copyWith(
-                              isFavorite: !foods[widget.index].isFavorite,
-                            );
-                          }),
-                          size: size.width * 0.09,
-                        ),
-                      ],
-                    ),
+        child: CustomScrollView(
+          slivers: [
+            SliverPadding(
+              padding: const EdgeInsetsGeometry.all(10),
+              sliver: SliverAppBar(
+                surfaceTintColor: Colors.transparent,
+                pinned: true,
+                leading: IconButton(
+                  icon: Icon(
+                    Icons.arrow_circle_left_outlined,
+                    size: size.width * 0.1,
                   ),
-
-                  SliverToBoxAdapter(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(height: size.height * 0.035),
-                          Text(
-                            foods[widget.index].subTitle,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 20,
-                            ),
-                          ),
-                          SizedBox(height: size.height * 0.01),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              SvgPicture.asset('assets/icons/star.svg'),
-                              SizedBox(width: size.width * 0.013),
-                              Text(
-                                '${foods[widget.index].rating} - ${foods[widget.index].prepTime} mins',
-                                style: const TextStyle(
-                                  color: AppColors.fourthColor,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: size.height * 0.02),
-                          Text(foods[widget.index].description),
-                          SizedBox(height: size.height * 0.02),
-                          Row(
-                            children: [
-                              SpicyIndicator(size: size),
-                              const Spacer(),
-                              PoritionCounter(size: size),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.asset(
+                    foods[widget.index].imgPath,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                expandedHeight: size.height * 0.24,
+                actions: [
+                  FavButton(
+                    isFavorite: foods[widget.index].isFavorite,
+                    onPressed: () => setState(() {
+                      foods[widget.index] = foods[widget.index].copyWith(
+                        isFavorite: !foods[widget.index].isFavorite,
+                      );
+                    }),
+                    size: size.width * 0.09,
                   ),
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                bottom: 30.0,
-                top: 10,
-                left: 20,
-                right: 20,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: AppColors.primaryColor,
-                      borderRadius: BorderRadius.circular(10),
-                      shape: BoxShape.rectangle,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        '\$${foods[widget.index].price}',
-                        style: const TextStyle(color: Colors.white),
+        
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: size.height * 0.035),
+                    Text(
+                      foods[widget.index].subTitle,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
                       ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.secondaryColor,
-                      foregroundColor: Colors.white,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
+                    SizedBox(height: size.height * 0.01),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SvgPicture.asset('assets/icons/star.svg'),
+                        SizedBox(width: size.width * 0.013),
+                        Text(
+                          '${foods[widget.index].rating} - ${foods[widget.index].prepTime} mins',
+                          style: const TextStyle(
+                            color: AppColors.fourthColor,
+                          ),
+                        ),
+                      ],
                     ),
-                    child: const Text('ORDER NOW'),
-                  ),
-                ],
+                    SizedBox(height: size.height * 0.02),
+                    Text(foods[widget.index].description),
+                    SizedBox(height: size.height * 0.02),
+                    Row(
+                      children: [
+                        SpicyIndicator(size: size),
+                        const Spacer(),
+                        PoritionCounter(size: size),
+                      ],
+                    ),
+                  ],
+                ),
               ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(
+          bottom: 30.0,
+          top: 10,
+          left: 20,
+          right: 20,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            DecoratedBox(
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.circular(10),
+                shape: BoxShape.rectangle,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  '\$${foods[widget.index].price}',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.secondaryColor,
+                foregroundColor: Colors.white,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+              child: const Text('ORDER NOW'),
             ),
           ],
         ),
